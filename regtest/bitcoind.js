3fc2252eeaa621fe04b1357a1b981c5eb71a8a62
Sotiris Blad <sotiris2009@hotmail.com>
2018-08-17 14:34:16 +0300

    Update bitcoind.js

--- regtest/bitcoind.js
+++ regtest/bitcoind.js
@@ -7,7 +7,7 @@ var index = require('..');
 var log = index.log;
 
 var chai = require('chai');
-var bitcore = require('bitcore-lib-terracoin');
+var bitcore = require('bitcore-lib-mue');
 var BN = bitcore.crypto.BN;
 var async = require('async');
 var rimraf = require('rimraf');
@@ -17,7 +17,7 @@ var bitcoind;
 var should = chai.should();
 var assert = chai.assert;
 var sinon = require('sinon');
-var BitcoinRPC = require('bitcoind-rpc-terracoin');
+var BitcoinRPC = require('bitcoind-rpc-mue');
 var transactionData = [];
 var blockHashes = [];
 var utxos;
@@ -46,7 +46,7 @@ describe('Bitcoind Functionality', function() {
       bitcoind = require('../').services.Bitcoin({
         spawn: {
           datadir: datadir,
-          exec: path.resolve(__dirname, process.env.HOME, './.bitcore/data/terracoind')
+          exec: path.resolve(__dirname, process.env.HOME, './.bitcore/data/mued')
         },
         node: {
           network: regtestNetwork,
